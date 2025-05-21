2b4ddf9dfdbf154a002e8f85e0d7d1ff707b9f88
sourabhkushwah1988 <sourabhkushwah1988@gmail.com>
2025-05-21 15:44:49 +0530

    inital commet

---
+++ GENUIN_CODE/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('blog/', views.blog, name='blog'),
+    path('contact/', views.contact, name='contact'),
+    path('course_inner/', views.course_inner, name='course_inner'),
+    path('course/', views.course, name='course'),
+    path('home/', views.home, name='home'),
+    path('post/', views.post, name='post'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('base/',views.base,name='base'),
+    path('learnmore_home/',views.learnmore_home,name='learnmore_home'),
+    path('success/', views.success, name='success'), 
+    path('my_course/', views.my_course, name='my_course'), 
+    path('update-profile/', views.update_profile, name='update_profile'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+
+]
