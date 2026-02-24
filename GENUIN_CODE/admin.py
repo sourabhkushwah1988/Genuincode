8e58629f3fc9cc622970c6cd1b7bec5fbe60d134
sourabh <sourabh@example.com>
2026-02-25 00:06:29 +0530

    update

--- GENUIN_CODE/admin.py
+++ GENUIN_CODE/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 # 🔁 Update names below if models are renamed
-from .models import Ragister, Contact, CustomUser, LoginAttempt
+from .models import Ragister, Contact, CustomUser, LoginAttempt, Profile
 
 # 🔁 Change names if renamed
 admin.site.register(Ragister)
 admin.site.register(Contact)
 admin.site.register(CustomUser)
 admin.site.register(LoginAttempt)
+admin.site.register(Profile)
